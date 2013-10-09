ca0a921076f2f85ccfb8bffbd712b9d15f2ab1ab
Huber Flores <huber@amatista.(none)>
2013-10-09 21:51:40 +0300

    Common utitilies modified

--- GCMapp/src/com/example/gcmapp/CommonUtilities.java
+++ GCMapp/src/com/example/gcmapp/CommonUtilities.java
@@ -4,21 +4,22 @@ import android.content.Context;
 import android.content.Intent;
 
 
+/*
+ * SENDER_ID = https://code.google.com/apis/console/#project:662678XXXXXX:services
+ */
+
 
 public final class CommonUtilities {
 
-static final String SENDER_ID = "xxxxxxxxxxxxxx";
-
+//Modify this two variables	
+static final String SENDER_ID = "662678XXXXXX";
 static final String SERVER_URL = "http://ec2-xxx-xx-xx-xx.compute-1.amazonaws.com:8080/gcm-demo";
 
 static final String TAG = "GCMPushNotification";
 
-static final String DISPLAY_MESSAGE_ACTION =
-    "com.example.gcmapp.DISPLAY_MESSAGE";
-
+static final String DISPLAY_MESSAGE_ACTION = "com.example.gcmapp.DISPLAY_MESSAGE";
 
 static final String EXTRA_MESSAGE = "message";
-
 
 static void displayMessage(Context context, String message) {
 	Intent intent = new Intent(DISPLAY_MESSAGE_ACTION);
